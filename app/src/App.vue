036419f364b9d1a2e1b6ff0e20b6b120104856e7
Lyubov Bormotova <l.bormotova@leadgid.ru>
2021-02-21 20:56:18 +0300

    init 3d

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div ref="canvas"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as THREE from 'three'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      winW: window.innerWidth || 100,
+      winH: window.innerHeight || 100,
+      // локальная камера
+      fov: 75,
+    };
+  },
+  methods: {
+    init() {
+      //   1. Создаем сцену
+      this.scene = new THREE.Scene();
+      this.scene.background = new THREE.Color(0xcccccc);
+      
+      this.camera = new THREE.PerspectiveCamera(this.fov, this.winW / this.winH, 1, 1000);
+      this.camera.position.z = 5;
+      this.camera.position.y = 1;
+      this.camera.position.x = 1;
+      
+      //   вспомогательные оси x, y, z
+      let axes = new THREE.AxesHelper(5);
+      this.scene.add(axes);
+      this.donut = new this.Torus(0x562354);
+      this.scene.add(this.donut);
+      
+      //   3. Создаем визуализатор (renderer)
+      this.renderer = new THREE.WebGLRenderer();
+      this.renderer.setSize(this.winW, this.winH);
+      
+      this.$refs.canvas.appendChild(this.renderer.domElement); // добавляем визаулизатор в DOM
+    },
+    loop() {
+      this.renderer.render(this.scene, this.camera); // рендер сцены
+      this.donut.rotation.y -= .01;
+      this.donut.rotation.z += .01;
+      this.donut.rotation.x += .01;
+
+      requestAnimationFrame(this.loop);
+    },
+    Torus(color = 0xcccccc) {
+      // радиус кольца, толщина кольца, кол-во сегментов создающих толщину, кол-во сегментов создающих круг, арка
+      const geometry = new THREE.TorusGeometry(1,.4,10,20,2 * Math.PI);
+      const material = new THREE.MeshBasicMaterial({ color: color,
+                                                   wireframe: true});
+      const torus = new THREE.Mesh(geometry, material);
+      return torus;
+    },
+  },
+  mounted() {
+    this.init();
+    this.loop();
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import 'reset-css';
 </style>
